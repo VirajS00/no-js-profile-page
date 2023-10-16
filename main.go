69600290976864b3288982a79fa98380af592079
Viraj Shukla <virajshukla00@gmail.com>
2023-10-17 00:19:12 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Static("/assets", "./views/assets")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"name": "Viraj Shukla",
+		})
+	})
+
+	app.Listen(":3000")
+}
